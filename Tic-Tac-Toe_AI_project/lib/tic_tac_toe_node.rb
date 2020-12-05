5ce2e027b6a8a3d2520fd3726747ce255537a7e3
Alex <alex.ortiz9481@gmail.com>
2020-12-05 14:48:52 -0500

    Wrote the initialize method that takes in an instance of  Board, next_mover_mark, and prev_move_pos if given

--- Tic-Tac-Toe_AI_project/lib/tic_tac_toe_node.rb
+++ Tic-Tac-Toe_AI_project/lib/tic_tac_toe_node.rb
@@ -2,6 +2,9 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +16,23 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children = []
+
+
   end
+
+  def empty_positions
+    empty_positions = []
+    childen = []
+    row = 0
+    while row <= 2
+      (0..2).each do |col|
+        position = [row, col]
+        empty_positions << position if @board.empty?(position)
+      end
+      row += 1
+    end
+    empty_positions
+  end
+
 end
