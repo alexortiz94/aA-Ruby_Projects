41a157c63eeb76267b69e6be29ae2ad91d1dff22
Alex <alex.ortiz9481@gmail.com>
2020-12-05 19:22:55 -0500

    Created a subclass of ComputerPlayer and overwrote their move method to play tic-tac-toe optimally and can never lose

--- Tic-Tac-Toe_AI_project/lib/super_computer_player.rb
+++ Tic-Tac-Toe_AI_project/lib/super_computer_player.rb
@@ -2,6 +2,15 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    move_tree = TicTacToeNode.new(game.board, mark)
+    child_nodes = move_tree.children
+    child_nodes.each do |child_node|
+      return child_node.prev_move_pos if child_node.winning_node?(mark)
+    end
+    child_nodes.each do |child_node|
+      return child_node.prev_move_pos if !child_node.losing_node?(mark)
+    end
+    raise "Error"
   end
 end
 
