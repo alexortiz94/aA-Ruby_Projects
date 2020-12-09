337ca995031d53e554ab7cfd6ebf6afc542217b7
Alex <alex.ortiz9481@gmail.com>
2020-12-09 01:17:14 -0500

    Wrote the #initialize, #place_stones, and #valid_move? methods that passed the specs

--- mancala_homework/lib/board.rb
+++ mancala_homework/lib/board.rb
@@ -2,13 +2,25 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @player_one_name = name1
+    @player_two_name = name2
+    @cups = Array.new(14) { Array.new }
+    
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, cup_idx|
+      4.times { |i| cup << :stone } unless cup_idx == 6 || cup_idx == 13
+    end 
   end
 
   def valid_move?(start_pos)
+    valid_cups = (0..5).to_a + (7..12).to_a
+    raise "Invalid starting cup" unless valid_cups.include?(start_pos)
+    raise "Starting cup is empty" if @cups[start_pos].empty?
+    true 
   end
 
   def make_move(start_pos, current_player_name)
