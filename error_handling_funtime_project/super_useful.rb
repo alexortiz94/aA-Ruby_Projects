a7854d295523ea9062215a5a775f017285a4f389
Alex <alex.ortiz9481@gmail.com>
2020-12-10 12:56:18 -0500

    Completed Error Handling Funtime project

--- error_handling_funtime_project/super_useful.rb
+++ error_handling_funtime_project/super_useful.rb
@@ -1,16 +1,20 @@
-# PHASE 2
+require 'numbers_in_words'
+
+# PHASE 0
 def convert_to_int(str)
-  Integer(str)
+  NumbersInWords.in_numbers(str)
 end
 
-# PHASE 3
+# PHASE 1
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise ArgumentError  
   else 
-    raise StandardError 
+    raise NameError 
   end 
 end
 
@@ -20,18 +24,29 @@ def feed_me_a_fruit
   puts "Feed me a fruit! (Enter the name of a fruit:)"
   maybe_fruit = gets.chomp
   reaction(maybe_fruit) 
+rescue NameError => exception
+  puts "That is not a fruit!"
+rescue ArgumentError => exception
+  puts "That is not a fruit, but I could always use a cup-a-joe"
+  retry
 end  
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
     @name = name
+    raise ArgumentError.new "name cannot be left blank" if name.length <= 0
     @yrs_known = yrs_known
+    raise ArgumentError.new "best friends must have known each other for at least 5 years" if yrs_known < 5
     @fav_pastime = fav_pastime
+    raise ArgumentError.new "best friends have to have a favorite pastime" if fav_pastime.length <= 0
+  rescue ArgumentError => e
+    puts "Couldn't initialize friendship"
+    puts "Because #{e.message}"
   end
 
   def talk_about_friendship
-    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+    puts "Wowza, we've been friends for #{@yrs_known} years. Let's be friends for another #{1000 * @yrs_known} years."
   end
 
   def do_friendstuff
