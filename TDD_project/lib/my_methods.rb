89d154dd0a09936b916acb4afced13b21638baf1
Alex <alex.ortiz9481@gmail.com>
2021-01-19 19:40:06 -0500

    Completed Array portion of project with all specs passing

--- TDD_project/lib/my_methods.rb
+++ TDD_project/lib/my_methods.rb
@@ -23,6 +23,36 @@ class Array
         zero_sums
     end
 
+    def my_transpose
+        transposed = []
+        (0..2).each do |col_idx|
+            col = []
+            self.each do |row| 
+    
+                col << row[col_idx]
+            end
+
+            transposed[col_idx] = col
+        end
+        transposed
+    end
+
+    def stock_picker
+        raise "all elements must be numbers" unless self.all? { |el| el.is_a?(Integer) }
+        high = self.max
+        low = nil
+        self.each_with_index do |el, idx|
+            next_el = self[idx + 1]
+            break if next_el ==  high
+            
+            low ||= (el < next_el) ? el : next_el
+            low = el if el < low
+        end
+        buy_and_sell_days = [ self.index(low) , self.index(high) ]
+    end
+
+
+
 
 end
 
--- TDD_project/spec/my_methods_spec.rb
+++ TDD_project/spec/my_methods_spec.rb
@@ -22,4 +22,29 @@ describe Array do
             expect(my_array.two_sum).to match_array([[0,4], [2,3]])
         end
     end
+
+    describe "#my_transpose" do
+        it "takes a two-dimensional grid, and transposes the rows with the columns" do
+            my_array = [
+                [0,1,2],
+                [3,4,5],
+                [6,7,8]
+            ]
+            expect(my_array.my_transpose).to match_array([[0,3,6],[1,4,7],[2,5,8]])
+        end
+    end
+
+    describe "#stock_picker" do
+        my_array = [67, 68, 66, 72, 75, 85, 91, 88, 87] 
+        it "takes in an array of stock prices on different days" do
+            expect{ my_array.stock_picker }.to_not raise_error("all elements must be numbers")
+        end
+
+        it "returns the most profitable pair of days to buy, and then sell the stock" do
+            expect(my_array.stock_picker).to match_array([2, 6])
+        end
+
+    end
+        
+
 end
